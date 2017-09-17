9d79eaf536ebaf9a7b4c46225097f437b97427e4
Minh Tri <leminhtr@etu.utc.fr>
2017-09-17 00:37:03 -0500

    fix firebase 404; create class ToDoManager; begin CRUD method for  Manager class

--- src/app/to-do-manager/to-do-manager.component.ts
+++ src/app/to-do-manager/to-do-manager.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import {listToDo} from '../../list-toDo';
+import {ToDoService} from '../to-do.service';
+import {ToDo} from '../to-do';
 
 @Component({
   selector: 'app-to-do-manager',
   templateUrl: './to-do-manager.component.html',
-  styleUrls: ['./to-do-manager.component.css']
+  styleUrls: ['./to-do-manager.component.css'],
+  providers: [ToDoService]
 })
 export class ToDoManagerComponent implements OnInit {
 
-  constructor() { }
+  ToDoManager: listToDo[];
+  selectedListToDo: listToDo;
 
-  ngOnInit() {
+  constructor(
+    private todoService: ToDoService
+  ) { }
+
+  getToDoManager(): void {  // get data from ToDo service, using its method
+    // sets the component's heroes property to the array of heroes returned by the service.
+    this.todoService.getToDoManager().then(TDManager => this.ToDoManager = TDManager);
   }
+  ngOnInit() {
+    this.getToDoManager();
+  }
+
+  onSelect(listTD: listToDo): void {
+    this.selectedListToDo = listTD;
+  }
+
+  addList(listName: string): void {
+    listName = listName.trim();
+    if (!listName) { return; }
+    this.todoService.createList(listName)
+      .then(listToDo => {
+        this.ToDoManager.push(listToDo);
+        this.selectedListToDo = null;
+      });
+  }
+
+  deleteList(list: listToDo): void {
+    this.todoService
+      .delete(list.id)
+      .then(() => {
+        this.ToDoManager = this.ToDoManager.filter(l => l !== list);
+        if (this.selectedListToDo === list) { this.selectedListToDo = null; }
+      });
+  }
+
+
+
+
+
+
 
 }
---
+++ src/list-toDo.ts
@@ -0,0 +1,8 @@
+import {ToDo} from "./app/to-do";
+
+export class listToDo{
+  id: number;
+  name: string;
+  listToDo: ToDo[];
+  isAllDone: boolean;
+}
